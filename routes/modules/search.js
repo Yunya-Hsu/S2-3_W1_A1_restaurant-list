719de9b820cafe44d306eceaa7fd027ddbaaf845
Yunya Hsu <yunyahsu110@gmail.com>
2022-05-24 17:47:45 +0800

    refactor: modify search function by using mongoose commands

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -5,16 +5,31 @@ const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
-  const adjustedKeyword = keyword.trim().toLowerCase()
-  Restaurant.find()
+  Restaurant.find({
+    $or: [
+      { name: { $regex: keyword, $options: '$i' } },
+      { description: { $regex: keyword, $options: '$i' } }
+    ]
+  })
     .lean()
     .then(restaurants => {
-      const searchResult = restaurants.filter(item =>
-        item.name.toLowerCase().includes(adjustedKeyword) ||
-        item.description.toLowerCase().includes(adjustedKeyword))
-      res.render('index', { restaurants: searchResult, keyword })
+      res.render('index', { restaurants, keyword })
     })
     .catch(error => console.error(error))
 })
+
+// router.get('/', (req, res) => {
+//   const keyword = req.query.keyword
+//   const adjustedKeyword = keyword.trim().toLowerCase()
+//   Restaurant.find()
+//     .lean()
+//     .then(restaurants => {
+//       const searchResult = restaurants.filter(item =>
+//         item.name.toLowerCase().includes(adjustedKeyword) ||
+//         item.description.toLowerCase().includes(adjustedKeyword))
+//       res.render('index', { restaurants: searchResult, keyword })
+//     })
+//     .catch(error => console.error(error))
+// })
 
 module.exports = router
