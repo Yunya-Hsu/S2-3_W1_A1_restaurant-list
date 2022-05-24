ade870b76e97e0aba33c082477a19f6d0accdee6
Yunya Hsu <yunyahsu110@gmail.com>
2022-05-24 20:26:12 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,11 +4,33 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
+const sortDetail = [
+  { _id: 'asc' },
+  { name: 'asc' },
+  { name: 'desc' },
+  { rating: 'desc' },
+  { category: 'asc' },
+  { location: 'asc' }
+]
+
+const sortName = [
+  "預設",
+  "餐廳名稱：A → Z",
+  "餐廳名稱：Z → A",
+  "評分：高 → 低",
+  "類別",
+  "地區"
+]
+
 router.get('/', (req, res) => {
+  const selectedSort = Number(req.query.OM) || 0
+  const sortType = sortName[selectedSort]
   Restaurant.find()
     .lean()
-    .sort({ _id: 'asc' })
-    .then(restaurants => res.render('index', { restaurants }))
+    .sort(sortDetail[selectedSort])
+    .then(restaurants => {
+      res.render('index', { restaurants, sortType })
+    })
     .catch(error => console.error(error))
 })
 
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -18,18 +18,5 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// router.get('/', (req, res) => {
-//   const keyword = req.query.keyword
-//   const adjustedKeyword = keyword.trim().toLowerCase()
-//   Restaurant.find()
-//     .lean()
-//     .then(restaurants => {
-//       const searchResult = restaurants.filter(item =>
-//         item.name.toLowerCase().includes(adjustedKeyword) ||
-//         item.description.toLowerCase().includes(adjustedKeyword))
-//       res.render('index', { restaurants: searchResult, keyword })
-//     })
-//     .catch(error => console.error(error))
-// })
 
 module.exports = router
