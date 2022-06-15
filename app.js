d1262088aee8459d7a3b9d4f517e5f445c0e0940
Yunya Hsu <yunyahsu110@gmail.com>
2022-06-15 14:08:55 +0800

    feat: add express-session, login router & login page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const port = 3000
 const routers = require('./routes')
@@ -13,6 +14,12 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// middleware
+app.use(session({
+   secret: 'asdfghjkasdfasdf',
+   resave: false,
+   saveUninitialized: true
+})) // 設定session
 app.use(express.urlencoded({ extended: true })) // 設定body-parser(解析post傳回來的req，body-parser已包在express中)
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過method-override進行前置處理
 app.use(express.static('public')) // 設定靜態資料使用public資料夾
